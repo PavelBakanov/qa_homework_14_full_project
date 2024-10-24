7caa29885faa80f805bf58388e33b9371378c23e
PavelBakanov <maxconghunter@mail.ru>
2024-10-24 16:52:45 +0300

    minor changes

--- src/test/java/tests/CrowdTestingTests.java
+++ src/test/java/tests/CrowdTestingTests.java
@@ -33,8 +33,8 @@ public class CrowdTestingTests extends TestBase {
             mainPage.clickTestingButton();
         });
 
-        step("Ищем на странице текст в определенном месте", () -> {
-
+        step("Проверяем по тексту всё меню 'О компании'", () -> {
+            testingPage.checkTextInAboutCompanyMenu();
         });
 
     }
