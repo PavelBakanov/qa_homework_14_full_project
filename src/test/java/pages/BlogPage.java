fc8b31231500dc98619dd444916f732fe03db3b7
PavelBakanov <maxconghunter@mail.ru>
2024-10-25 14:10:47 +0300

    minor changes

--- src/test/java/pages/BlogPage.java
+++ src/test/java/pages/BlogPage.java
@@ -11,12 +11,12 @@ public class BlogPage {
     }
 
     public BlogPage typeTextToInputField() {
-        $("[name='stext']").setValue("как получить").pressEnter();
+        $("[name='stext']").scrollTo().setValue("как получить").pressEnter();
         return this;
     }
 
     public BlogPage checkCertainTextInPlain() {
-        $(".plain").shouldHave(text(
+        $(".plain").scrollTo().shouldHave(text(
                 "Как получить дополнительные бонусы за участие в проектах краудтестирования"));
         return this;
     }
