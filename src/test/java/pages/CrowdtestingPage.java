55f129e379830baa4a9775a7aec9002fe6ff5e08
PavelBakanov <maxconghunter@mail.ru>
2024-10-28 16:43:43 +0300

    minor changes

--- src/test/java/pages/CrowdtestingPage.java
+++ src/test/java/pages/CrowdtestingPage.java
@@ -3,6 +3,8 @@ package pages;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 
+import java.util.Objects;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.actions;
@@ -15,15 +17,22 @@ public class CrowdtestingPage {
         String stringBrowserWidth = Configuration.browserSize;
         String[] arrayForSplit = stringBrowserWidth.split("x");
         int browserWidth = Integer.parseInt(arrayForSplit[1]);
-        actions().dragAndDropBy(sliderLocator, - browserWidth / 3, 0).perform();
+        actions().dragAndDropBy(sliderLocator, -browserWidth / 3, 0).perform();
         return this;
     }
 
     public CrowdtestingPage checkTextInCurrentSlide() {
-        $(".slider.js-crowdtesting-new-feedbacks-slider").$("[aria-hidden='false'] .txt").
-                shouldHave(text("Опыт сотрудничества с компанией CrowdTesting" +
-                        " наша компания характеризует исключительно положительно. " +
-                        "Все работы выполняются с высоким качеством, в установленные сроки."));
+        if (Objects.equals(Configuration.browserSize, "800x600")) {
+            $(".slider.js-crowdtesting-new-feedbacks-slider").$("[aria-hidden='false'] .txt").
+                    shouldHave(text("Компания CrowdTesting за время нашего сотрудничества неоднократно " +
+                            "продемонстрировала свой высокий профессионализм и компетентность в " +
+                            "исследованиях юзабилити и клиентского опыта наших сервисов."));
+        } else {
+            $(".slider.js-crowdtesting-new-feedbacks-slider").$("[aria-hidden='false'] .txt").
+                    shouldHave(text("Опыт сотрудничества с компанией CrowdTesting" +
+                            " наша компания характеризует исключительно положительно. " +
+                            "Все работы выполняются с высоким качеством, в установленные сроки."));
+        }
         return this;
     }
 }
