b5e21d910f520bf0257161b917bef42c8b951e30
PavelBakanov <maxconghunter@mail.ru>
2025-01-05 19:01:04 +0300

    minor changes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -28,10 +28,7 @@ public class TestBase {
         Configuration.browser = webConfig.getBrowserName();//System.getProperty("browser");
         Configuration.browserVersion = webConfig.getBrowserVersion();//System.getProperty("browserVersion");
         Configuration.baseUrl = webConfig.getBaseUrl();//System.getProperty("baseURL");
-        if (System.getProperty("env").equals(null))
-        {
-            Configuration.remote = "https://" + webConfig.getLogin() + "@" + webConfig.getRemoteUrl();
-        }
+        Configuration.remote = "https://" + webConfig.getLogin() + "@" + webConfig.getRemoteUrl();
         //Configuration.remote = "https://" + System.getProperty("login") + "@" + System.getProperty("remoteURL");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
