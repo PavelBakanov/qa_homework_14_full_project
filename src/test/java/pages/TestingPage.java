e3621a06c3be72d0a83f87dd5397da542c3f72e1
Pavel Bakanov <88797455+PavelBakanov@users.noreply.github.com>
2025-01-13 18:44:37 +0300

    Update TestingPage.java

--- src/test/java/pages/TestingPage.java
+++ src/test/java/pages/TestingPage.java
@@ -23,8 +23,8 @@ public class TestingPage {
     public TestingPage checkTextInAboutCompanyMenu(String... listMenuText) {
         aboutCompanyLocator.hover();
 
-        for(int i = 0; i < listMenuText.size(); i++) {
-            menuItemsLocator.get(i).shouldHave(text(listMenuText.get(i)));
+        for(int i = 0; i < listMenuText.length; i++) {
+            menuItemsLocator.get(i).shouldHave(text(listMenuText[i]));
         }
 
         
