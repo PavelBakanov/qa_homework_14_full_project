570b165cad5d46105b03a0907fabe8c079ecd673
Pavel Bakanov <88797455+PavelBakanov@users.noreply.github.com>
2025-01-13 19:28:20 +0300

    Update TestingPage.java

--- src/test/java/pages/TestingPage.java
+++ src/test/java/pages/TestingPage.java
@@ -12,6 +12,7 @@ public class TestingPage {
 
     private final SelenideElement textContainerLocator = $(".text.aos-init.aos-animate");
     private final SelenideElement aboutCompanyLocator = $$("a").findBy(text("О компании"));
+    private final SelenideElement burgerMenuLocator = $ ("a.open-nav");
     private final ElementsCollection menuItemsLocator = $("ul.sub").$$("li a");  
 
 
@@ -21,26 +22,16 @@ public class TestingPage {
     }
 
     public TestingPage checkTextInAboutCompanyMenu(String... listMenuText) {
+
+        if (burgerMenuLocator.exists()){
+            burgerMenuLocator.click();
+        }
+        
         aboutCompanyLocator.hover();
 
         for(int i = 0; i < listMenuText.length; i++) {
             menuItemsLocator.get(i).shouldHave(text(listMenuText[i]));
         }
-
-        
-        /*menuItemsLocator.get(0).shouldHave(text("О нас"))
-        menuItemsLocator.get(1).shouldHave(text("Портфолио"))
-        menuItemsLocator.get(2).shouldHave(text("Карьера"))
-        menuItemsLocator.get(3).shouldHave(text("Для респондентов"))*/
-        
-        
-        
-        /*$(byText("О компании")).scrollTo();
-        $(byText("О нас")).scrollTo();
-        $(byText("Портфолио")).scrollTo();
-        $(byText("Карьера")).scrollTo();
-        $(byText("Для респондентов")).scrollTo();*/
-
         return this;
     }
 }
