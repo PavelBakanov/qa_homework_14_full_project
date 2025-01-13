95cc0cffb7c4d0cdce3f0de51b2fbf5ce3df6319
Pavel Bakanov <88797455+PavelBakanov@users.noreply.github.com>
2025-01-13 20:02:17 +0300

    Update TestingPage.java

--- src/test/java/pages/TestingPage.java
+++ src/test/java/pages/TestingPage.java
@@ -12,9 +12,7 @@ public class TestingPage {
 
     private final SelenideElement textContainerLocator = $(".text.aos-init.aos-animate");
     private final SelenideElement aboutCompanyLocator = $$("a").findBy(text("О компании"));
-    private final SelenideElement burgerMenuLocator = $ ("a.open-nav");
     private final ElementsCollection menuItemsLocator = $("ul.sub").$$("li a");  
-
 
     public TestingPage findCertainText(String text) {
         textContainerLocator.shouldHave(text(text));
@@ -23,14 +21,10 @@ public class TestingPage {
 
     public TestingPage checkTextInAboutCompanyMenu(String... listMenuText) {
 
-        if (burgerMenuLocator.isDisplayed()){
-            burgerMenuLocator.click();
-        }
-        
-        aboutCompanyLocator.scrollIntoView(true).hover().click();
+        aboutCompanyLocator.hover();
 
         for(int i = 0; i < listMenuText.length; i++) {
-            menuItemsLocator.get(i).scrollIntoView(true).shouldHave(text(listMenuText[i]));
+            menuItemsLocator.get(i).shouldHave(text(listMenuText[i]));
         }
         return this;
     }
