604ae7162272251866c6e7c5092fd69a82656fea
Pavel Bakanov <88797455+PavelBakanov@users.noreply.github.com>
2025-01-13 20:17:23 +0300

    Update MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -12,30 +12,37 @@ public class MainPage {
     private final SelenideElement testingButtonLocator = $(byText("Тестировать"));
     private final SelenideElement researchButtonLocator = $(byText("Исследовать"));
 
+    @Step("Открываем главную страницу")
     public MainPage openMainPage() {
         open("https://www.crowdtesting.ru/");
         return this;
     }
 
+    @Step("Открыть раздел 'Блог'")
     public MainPage openBlogPage() {
         open("/blog/");
         return this;
     }
 
+    @Step("Открыть раздел 'Краудтестинг'")
     public MainPage openCrowdTestingPage() {
         open("/crowdtesting/");
         return this;
     }
+
+    @Step("Открыть раздел 'Кейсы'")
     public MainPage openCasesPage() {
         open("/company/cases/");
         return this;
     }
 
+    @Step("Нажать на кнопку 'Тестировать'")
     public MainPage clickTestingButton() {
         testingButtonLocator.scrollTo().pressEnter();
         return this;
     }
 
+    @Step("Нажать на кнопку 'Исследовать'")
     public MainPage clickResearchButton() {
         researchButtonLocator.scrollTo().pressEnter();
         return this;
