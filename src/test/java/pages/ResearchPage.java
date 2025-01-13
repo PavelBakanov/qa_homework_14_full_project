78d63b96065a9d3d16d384911aff91fd275056bc
Pavel Bakanov <88797455+PavelBakanov@users.noreply.github.com>
2025-01-13 20:06:55 +0300

    Update ResearchPage.java

--- src/test/java/pages/ResearchPage.java
+++ src/test/java/pages/ResearchPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
